a8cbe0d6cadf48b405419fa889b877cfc95f7daa
martin-wat-cresset <martin.wat@cresset-group.com>
2025-01-22 16:14:41 +0000

    Fix quotes being picked up by conda causing command error

--- setup.py
+++ setup.py
@@ -405,8 +405,10 @@ def conda_install(
             deps.append(dep)
 
     dependencies = deps
+    # FIXUP: Quotes being picked up by conda
+    fixup_deps = [dep.replace('"', '') for dep in dependencies]
 
-    cmd = [*conda_install, *dependencies]
+    cmd = [*conda_install, *fixup_deps]
     print("\nInstalling packages using:\n\n%s\n\n" % " ".join(cmd))
     status = subprocess.run(cmd)
 
