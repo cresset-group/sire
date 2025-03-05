0fa8ddd441d445e9482941d597d5b9949856e48e
Nigel Palmer <nigel.palmer@cresset-group.com>
2025-03-05 14:54:28 +0000

    Make sure box vectors are in reduced form. [ref #305]

--- corelib/src/libs/SireMove/openmmpmefep.cpp
+++ corelib/src/libs/SireMove/openmmpmefep.cpp
@@ -2985,7 +2985,11 @@ void OpenMMPMEFEP::createContext(IntegratorWorkspace &workspace, SireUnits::Dime
     // TriclinicBox
     else if (ptr_sys.property(space_property).isA<TriclinicBox>())
     {
-        const TriclinicBox &space = ptr_sys.property(space_property).asA<TriclinicBox>();
+        TriclinicBox space = ptr_sys.property(space_property).asA<TriclinicBox>();
+
+        // Make sure the box is in reduced form.
+        space.rotate();
+        space.reduce();
 
         // Get the three triclinic box vectors.
         // FIXME: not good practice of using auto here
