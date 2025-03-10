b63dc0c70da30aaf682cfe1e5e09bef1b1b3139e
Nigel Palmer <nigel.palmer@cresset-group.com>
2025-03-10 09:28:21 +0000

    Revert "Fix quotes being picked up by conda causing command error"
    This reverts commit aa91e90d794bdba237db33eacf8d9fa0e124eb7b.

--- setup.py
+++ setup.py
@@ -397,10 +397,8 @@ def conda_install(dependencies, install_bss_reqs=False, yes=True):
             deps.append(dep)
 
     dependencies = deps
-    # FIXUP: Quotes being picked up by conda
-    fixup_deps = [dep.replace('"', '') for dep in dependencies]
 
-    cmd = [*conda_install, *fixup_deps]
+    cmd = [*conda_install, *dependencies]
     print("\nInstalling packages using:\n\n%s\n\n" % " ".join(cmd))
     status = subprocess.run(cmd)
 
